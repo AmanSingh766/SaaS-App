339a934a036a599c37f3c0c1f51851665e63a757
AmanSingh766 <challengersaman2003@gmail.com>
2025-07-10 09:14:58 +0530

    Implement Auth,Billing ,database Setup,Create Companion Server Actions

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,9 +1,10 @@
+import {PricingTable} from "@clerk/nextjs";
 
-
-const Subscription =() => {
-    return(
-    <div> Subscription</div>
+const Subscription = () => {
+    return (
+        <main>
+            <PricingTable />
+        </main>
     )
-
 }
 export default Subscription
